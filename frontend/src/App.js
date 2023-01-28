5dcd284b05cc739e8ed07700a3daec46a0674aef
Taro Ishihara <taro.ishihara.mail@gmail.com>
2023-01-28 13:15:38 -0800

    add-zoom-sdk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,23 @@ import "./App.css";
 
 import { ContentsProvider } from "./context/ContentsContext.js";
 import ContentsManager from "./components/ContentsManager.js";
+import VideoCallButton from "./components/VideoCallButton";
 
 function App() {
+  const videoCallButtonPosition = {
+    position: "absolute",
+    top: "86%",
+    left: "82%",
+  };
+
   return (
     <div className="App">
       <ContentsProvider>
         <ContentsManager />
       </ContentsProvider>
+      <div style={videoCallButtonPosition}>
+        <VideoCallButton />
+      </div>
     </div>
   );
 }

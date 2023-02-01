afb313754e3acffa441592f5ed36d3afe6e87f95
Taro Ishihara <taro.ishihara.mail@gmail.com>
2023-01-31 20:28:50 -0800

    add video call function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,42 @@
 import "./App.css";
 
+import { useState } from "react";
+
 import { ContentsProvider } from "./context/ContentsContext.js";
 import ContentsManager from "./components/ContentsManager.js";
 import VideoCallButton from "./components/VideoCallButton";
 
 function App() {
+  const [callStarted, setCallStarted] = useState(false);
+
   const videoCallButtonPosition = {
     position: "absolute",
     top: "86%",
     left: "82%",
+  };
+
+  const selfViewStyle = {
+    position: "absolute",
+    bottom: "0px",
+    left: "320px",
+    height: "360px",
+    width: "480px",
+  };
+
+  const selfViewHidden = {
+    visibility: "hidden"
+  };
+
+  const participantViewStyle = {
+    position: "absolute",
+    top: "0px",
+    left: "0px",
+    height: "1080px",
+    width: "1920px",
+  };
+
+  const participantViewHidden = {
+    visibility: "hidden"
   };
 
   return (
@@ -17,8 +45,10 @@ function App() {
         <ContentsManager />
       </ContentsProvider>
       <div style={videoCallButtonPosition}>
-        <VideoCallButton />
+        <VideoCallButton setCallStarted={setCallStarted} />
       </div>
+      <canvas id="participant-canvas" style={callStarted ? participantViewStyle: participantViewHidden}></canvas>
+      <video id="self-view" style={callStarted ? selfViewStyle: selfViewHidden}></video>
     </div>
   );
 }
--- frontend/src/components/VideoCallButton.js
+++ frontend/src/components/VideoCallButton.js
@@ -1,29 +1,59 @@
 import { useState, useEffect } from "react";
 import ZoomVideo from "@zoom/videosdk";
 
-const VideoCallButton = () => {
-  const [zoomClient, setZoomClient] = useState()
+const ZOOM_TOKEN = process.env.REACT_APP_ZOOM_TOKEN;
+
+const VideoCallButton = ({ setCallStarted }) => {
+  const [zoomClient, setZoomClient] = useState();
 
   const startVideoCall = () => {
-    console.log(zoomClient);
+    const topic = "MYTOPIC";
+    const token = ZOOM_TOKEN
+    console.log(token)
+    const userName = "user1";
+
+    zoomClient
+      .join(topic, token, userName)
+      .then(() => {
+        const stream = zoomClient.getMediaStream();
+        setCallStarted(true);
+        stream.startAudio();
+
+        zoomClient.on("auto-play-audio-failed", () => {
+          console.log("auto play failed, waiting for a user interaction");
+        });
+        stream.startVideo({
+          videoElement: document.querySelector("#self-view"),
+        });
+        zoomClient.on("peer-video-state-change", (payload) => {
+          if (payload.action === "Start") {
+            stream.renderVideo(
+              document.querySelector("#participant-canvas"),
+              payload.userId,
+              1920,
+              1080,
+              0,
+              0,
+              2
+            );
+          } else if (payload.action === "Stop") {
+            stream.stopRenderVideo(
+              document.querySelector("#participant-canvas"),
+              payload.userId
+            );
+          }
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   useEffect(() => {
     const init = async () => {
       const client = ZoomVideo.createClient();
-      client.init('en-US', 'CDN')
-
-      const topic = ""
-      const token = ""
-      const userName = ""
- 
-      client.join(topic, token, userName, password).then(() => {
-        stream = client.getMediaStream()
-      }).catch((error) => {
-        console.log(error)
-      })
-
-      setZoomClient(client)
+      client.init("en-US", "CDN");
+      setZoomClient(client);
     };
     init();
     return () => {
@@ -43,9 +73,11 @@ const VideoCallButton = () => {
   };
 
   return (
-    <button style={videoCallButtonStyle} onClick={startVideoCall}>
-      Talk to stuff
-    </button>
+    <>
+      <button style={videoCallButtonStyle} onClick={startVideoCall}>
+        Talk to stuff
+      </button>
+    </>
   );
 };
 
